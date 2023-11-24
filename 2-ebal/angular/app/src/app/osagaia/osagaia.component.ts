1531d9588a24b87996b417cb80269a09bdefa3ac
AlainBasterra <basterra.alain@uni.eus>
2023-11-24 13:28:07 +0100

    angular

--- 2-ebal/angular/app/src/app/osagaia/osagaia.component.ts
+++ 2-ebal/angular/app/src/app/osagaia/osagaia.component.ts
@@ -7,21 +7,33 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './osagaia.component.html',
-  styleUrl: './osagaia.component.css'
+  styleUrls: ['./osagaia.component.css']
 })
-export class OsagaiaComponent{
+export class OsagaiaComponent {
 
   constructor() { }
-  
-  langilea = {
-    izena: 'Pepe',
-    abizena: 'Perez',
-    generoa: 'gizona',
-    irudia: '/assets/img/caraMujer.jpg'
-    };
-    
-    Title="Langileen zerrenda";
-    getTitle() {
+
+  // Método para incrementar los 'likes'
+  gehituLikes(langilea: any) {
+    langilea.likes++;
+  }
+
+  // Límite de filas para el *ngFor
+  rowLimit = 10; 
+
+  // Datos de ejemplo para los empleados
+  langileak = [
+    { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000, jaiotza: new Date(1980, 1, 1), likes: 0, lanean: true },
+    { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000, jaiotza: new Date(1985, 6, 15), likes: 0, lanean: true },
+    { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500, jaiotza: new Date(1990, 3, 20), likes: 0, lanean: true },
+    { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000, jaiotza: new Date(1975, 10, 30), likes: 0, lanean: true }
+  ];
+
+  // Título del componente
+  Title = "Langileen zerrenda";
+
+  // Método para obtener el título
+  getTitle() {
     return this.Title;
-    } 
+  } 
 }
