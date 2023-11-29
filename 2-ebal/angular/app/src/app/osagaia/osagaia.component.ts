d0eb90f44790941fc09340acbcc1721817671b49
AlainBasterra <basterra.alain@uni.eus>
2023-11-29 12:34:04 +0100

    ionic

--- 2-ebal/angular/app/src/app/osagaia/osagaia.component.ts
+++ 2-ebal/angular/app/src/app/osagaia/osagaia.component.ts
@@ -7,33 +7,30 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './osagaia.component.html',
-  styleUrls: ['./osagaia.component.css']
+  styleUrl: './osagaia.component.css'
 })
-export class OsagaiaComponent {
+export class OsagaiaComponent{
 
   constructor() { }
+  gehituLikes(langilea: any)
+  { langilea.likes++; }
 
-  // Método para incrementar los 'likes'
-  gehituLikes(langilea: any) {
-    langilea.likes++;
-  }
+  langilea = {
+    izena: 'Pepe',
+    abizena: 'Perez',
+    generoa: 'gizona',
+    irudia: '/assets/img/caraMujer.jpg'
+    };
 
-  // Límite de filas para el *ngFor
-  rowLimit = 10; 
-
-  // Datos de ejemplo para los empleados
   langileak = [
-    { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000, jaiotza: new Date(1980, 1, 1), likes: 0, lanean: true },
-    { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000, jaiotza: new Date(1985, 6, 15), likes: 0, lanean: true },
-    { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500, jaiotza: new Date(1990, 3, 20), likes: 0, lanean: true },
-    { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000, jaiotza: new Date(1975, 10, 30), likes: 0, lanean: true }
-  ];
-
-  // Título del componente
-  Title = "Langileen zerrenda";
-
-  // Método para obtener el título
-  getTitle() {
+    { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000, likes: 0, lanean: true },
+    { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000, likes: 0, lanean: true },
+    { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500, likes: 0, lanean: true },
+    { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000, likes: 0, lanean: true }
+    ];
+    myText = "Nire izena: <h1>aLAIN</h1> da";
+    Title="Langileen zerrenda";
+    getTitle() {
     return this.Title;
-  } 
+    } 
 }
