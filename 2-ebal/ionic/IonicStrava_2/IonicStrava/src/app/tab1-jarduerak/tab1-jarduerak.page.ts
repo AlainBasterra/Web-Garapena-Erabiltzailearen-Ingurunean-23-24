643e5379fcedcb1ee63cfb773481602210039bec
AlainBasterra <basterra.alain@uni.eus>
2024-01-10 11:37:25 +0100

    Eguneratu

---
+++ 2-ebal/ionic/IonicStrava_2/IonicStrava/src/app/tab1-jarduerak/tab1-jarduerak.page.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location} from '@angular/common';
+import { KlubaService } from '../services/kluba.service';
+import { Kluba } from '../classes/kluba';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-tab1-jarduerak',
+  templateUrl: './tab1-jarduerak.page.html',
+  styleUrls: ['./tab1-jarduerak.page.scss'],
+})
+export class Tab1JarduerakPage implements OnInit {
+
+  kluba = {} as Kluba;
+  apiService = {} as ApiService;
+
+  constructor(private klubaService: KlubaService, private route: ActivatedRoute, private location: Location) { }
+
+  /*getKluba(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.klubaService.getKluba(id)
+    .subscribe(kluba => {
+    this.kluba = kluba;
+    this.kluba.jarduerak.sort( (a, b): number => {
+    return (b.moving_time - a.moving_time);
+    });
+    },
+    error => console.log('Error :: ' + error));
+  }*/
+  getKluba(): void {
+    this.apiService.dbState().subscribe((res) => {
+      if (res) {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        this.apiService.fetchKluba(id).subscribe(
+          (kluba) => {
+            if (kluba) {
+              this.kluba = kluba;
+              this.kluba.jarduerak.sort((a, b): number => {
+                return b.moving_time - a.moving_time;
+              });
+            } else {
+              console.error(`Kluba with id ${id} not found.`);
+              // Handle the case where the Kluba is not found
+            }
+          },
+          (error) => console.error('Error fetching Kluba:', error)
+        );
+      }
+    });
+  }
+  
+  
+  
+   goBack(): void {
+    this.location.back();
+   }
+  ngOnInit() {
+    this.getKluba();
+  }
+
+}
